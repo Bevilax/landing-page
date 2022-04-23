fe18985c5bfc224cde7a3995e114dbbee5b163a0
Bevilax <matheusbevilacqua@hotmail.com>
2022-04-23 01:47:16 +0000

    test2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Home = () => {
+export const Home = () => {
 	return (
 		<div className="NavigationBar">
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -42,31 +42,28 @@ const Home = () => {
 					</ul>
 				</div>
 			</nav>
-		</div>,
-		<div className="jumbotron">
-		<h1 className="display-4">Hello, world!</h1>
-		<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-		<hr className="my-4"></hr>
-		<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-		<p className="lead">
-			<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-		</p>
-</div>
+		</div>
 	);
 };
 
-// const Jumbotron = () => {
-// 	return (
-// 		<div className="jumbotron">
-// 			<h1 className="display-4">Hello, world!</h1>
-// 			<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-// 			<hr className="my-4"></hr>
-// 			<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-// 			<p className="lead">
-// 				<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-// 			</p>
-// </div>
-// 	);
-// };
-
-export default Home;
+export const Jumbotron = () => {
+	return (
+		<div className="jumbotron">
+			<h1 className="display-4">Hello, world!</h1>
+			<p className="lead">
+				This is a simple hero unit, a simple jumbotron-style component
+				for calling extra attention to featured content or information.
+			</p>
+			<hr className="my-4"></hr>
+			<p>
+				It uses utility classes for typography and spacing to space
+				content out within the larger container.
+			</p>
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Learn more
+				</a>
+			</p>
+		</div>
+	);
+};
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import  {Home, Jumbotron} from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Home />, <Jumbotron />, document.querySelector("#app"));
